7505df323e3b282ded6e850aee5a29c3016fcb76
Daehwan Lee <redface0512@gmail.com>
2021-01-14 22:21:56 -0800

    comments added to impl EmailMessage

--- src/lib.rs
+++ src/lib.rs
@@ -35,17 +35,41 @@ impl Header {
     }
 }
 
-
+/// EmailMessage struct.
+/// It is mandatory to instantiate EmailMessage by EmailMessage::create
 pub struct EmailMessage<'a> {
     email_path: &'a str,
     json_path: &'a str,
 }
 
 impl<'a> EmailMessage<'a> {
+    /// Instantiate EmailMessage
+    ///
+    /// ```rust
+    /// use email_template::EmailMessage;
+    ///
+    /// fn main() {
+    ///    // Instantiate EmailMessage
+    ///    let email_template = EmailMessage::create("./email.txt","./data.json");
+    ///    println!("email_template: {:?}", email_template);
+    /// }
+    /// ```
     pub fn create(email_path: &'a str, json_path: &'a str) -> EmailMessage<'a> {
         EmailMessage { email_path, json_path }
     }
 
+    /// Build emails
+    ///
+    /// ```rust
+    /// use email_template::EmailMessage;
+    ///
+    /// fn main() {
+    ///    // Instantiate EmailMessage
+    ///    let email_template = EmailMessage::create("./email.txt","./data.json");
+    ///    let emails = email_template.build_emails();
+    ///    println!("emails: {:?}", emails);
+    /// }
+    /// ```
     pub fn build_emails(&self) -> Vec<Message> {
         let json_data = get_json_data(self.json_path);
         let contents = get_email_contents(self.email_path);
